a23fcec158b3132eb34f27b4f19630275ca7353a
Richel Leung <>
2019-09-25 11:11:04 -0700

    feat(TDD Tutorial): Heroku
    Heroku initialization
    
    [ICTRAINING-270]

--- superlists/settings.py
+++ superlists/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 's5#4+4$o!xu1p7=msb^u0bw2^2czcabml$%*dxx-04)bw=o09k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tdd-tutorial-heroku-app.herokuapp.com']
 
 
 # Application definition
@@ -120,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Activate Django-Heroku
+django_heroku.settings(locals())
